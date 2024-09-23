f5557b721daea708599846b42f77c9cc2a843b29
Peter99burrows <peter@maxsmartdigital.com>
2024-09-23 21:52:24 +0100

    Changes to fix DB Connect issue

--- MAaGI_Front_End_V2/Program.cs
+++ MAaGI_Front_End_V2/Program.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MAaGI_Front_End_V2;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -22,5 +26,17 @@ namespace MAaGI_Front_End_V2.Models
         {
             PromptText = string.Empty; // Initialize with a non-null value
         }
+    }
+}
+
+public partial class Program
+{  public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        await builder.Build().RunAsync();
     }
 }
