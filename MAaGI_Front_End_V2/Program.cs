330a542d28758d4cd0c133de6d163e83908a33bd
Peter99burrows <peter@maxsmartdigital.com>
2024-09-23 18:22:29 +0100

    First Full commit

--- MAaGI_Front_End_V2/Program.cs
+++ MAaGI_Front_End_V2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MAaGI_Front_End_V2;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Data.SqlClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,3 +12,15 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 await builder.Build().RunAsync();
 
+namespace MAaGI_Front_End_V2.Models
+{
+    public class QueryModel
+    {
+        [Required]
+        public string PromptText { get; set; }
+        public QueryModel()
+        {
+            PromptText = string.Empty; // Initialize with a non-null value
+        }
+    }
+}
